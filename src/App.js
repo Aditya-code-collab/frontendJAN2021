b1b2557d9def14dd149d5cc2b98e7813f63ca6d7
adi <19106@iiitu.ac.in>
2021-01-21 12:39:59 +0530

    added sign in page and style.css and header.js files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>Hallow World</h1>;
-}
+import signUp from "./pages/signUp";
+import signIn from "./pages/signin";
+import header from "./pages/header";
+import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <main>
+          <Route path="/" component={signUp} exact />
+          <Route path="/signin" component={signIn} exact />
+          <Route path="/home" component={header} exact />
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
+
+
+
+//background-image: url(https://opconventioncenter.com/wp-content/uploads/2018/02/EXPO-Inc.-tools.jpeg);
